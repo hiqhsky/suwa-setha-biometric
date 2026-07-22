3e03eea408e3da643269398d51046cec3d78bbb7
hiqhsky <muhammedaqeel2025@gmail.com>
2026-07-22 23:59:09 +0530

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import SuwaSethaBiometricSecurity from './App.jsx';   // Make sure this matches your filename
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SuwaSethaBiometricSecurity />
-  </React.StrictMode>
-);
